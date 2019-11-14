f077dc219aa3f389faa1c851ebde03edbfbe1ae7
TimTree <TimTree@users.noreply.github.com>
2019-11-13 16:44:37 -0800

    Submit assignment 3

--- FLASKAPP/app.py
+++ FLASKAPP/app.py
@@ -1,6 +1,8 @@
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
+from .socialmedia import add_or_update_user
+from .predict import predict_user
 
 #now we make a app factory
 
@@ -14,6 +16,34 @@ def create_app():
     def root():
         users = User.query.all()
         return render_template('base.html',title='Homepage',users=users)
+
+    # New route to add and get users
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, uimessage=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method=='POST':
+                add_or_update_user(name)
+                uimessage="User {} successfully added".format(name)
+            messages = User.query.filter(User.name == name).one().messages
+        except Exception as e:
+            uimessage = "Error adding {}: {}".format(name,e)
+            messages = []
+        return render_template('user.html', title=name, messages=messages,uimessage=uimessage)
+
+    @app.route('/compare', methods=['POST'])
+    def compare(uimessage=''):
+        user1, user2 = sorted([request.values['user1'],
+                               request.values['user2']])
+        if user1 == user2:
+            uimessage = 'Cannot compare a user to themselves'
+        else:
+            prediction = predict_user(user1, user2, request.values['tweet_text'])
+            uimessage = '"{}" is more likely to be said by {} than {}'.format(
+            request.values['tweet_text'], user1 if prediction else user2,
+            user2 if prediction else user1)
+        return render_template('prediction.html', title='Prediction', uimessage=uimessage)
 
     @app.route('/reset')
     def reset():
--- FLASKAPP/socialmedia.py
+++ FLASKAPP/socialmedia.py
@@ -6,3 +6,25 @@ from .models import DB, Message, User
 from .twitter_scraper import Profile, get_tweets
 
 BASILICA = basilica.Connection(config('BASILICA_KEY'))
+
+def add_or_update_user(username):
+    """Add or update a user and their tweets, otherwise error"""
+    try:
+        twitter_user=username
+        db_user=(User.query.get(int(Profile(twitter_user).userid)) or
+        User(id=int(Profile(twitter_user).userid), name=username))
+        DB.session.add(db_user)
+        tweets = list(get_tweets(twitter_user, pages=5,include_rts=False))
+        if tweets:
+            db_user.newest_tweet_id = int(tweets[0]['tweetId'])
+        for tweet in tweets:
+            #calculate embedding on the full tweet
+            embedding = BASILICA.embed_sentence(tweet['text'], model='twitter')
+            db_message = Message(id=tweet['tweetId'], text=tweet['text'], embedding=embedding)
+            DB.session.add(db_message)
+            db_user.messages.append(db_message)
+    except Exception as e:
+        print('Error processing {}: {}'.format(username,e))
+        raise e
+    else:
+        DB.session.commit()
